6f3a8bdf4c80bf0370be48ab6fc22806c370eda3
AJ Clarke <aj@ajmac.local>
2016-08-12 10:37:28 -0700

    Should return null, not false

--- wpex-post-types-feed-links-fix.php
+++ wpex-post-types-feed-links-fix.php
@@ -13,7 +13,7 @@ function wpex_post_types_feed_link_fix() {
 		$obj         = get_post_type_object( get_post_type() );
 		$has_archive = $obj->has_archive;
 		if ( empty( $has_archive ) ) {
-			add_filter( 'post_comments_feed_link', '__return_false' );
+			add_filter( 'post_comments_feed_link', '__return_null' );
 		}
 	}
 }
